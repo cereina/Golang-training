0a1aafc619ff2a68b7ec27cf067a2a77bc671394
cereina <kassemmazen@gmail.com>
2017-07-16 17:49:13 -0400

    ADD FILE 01-PACKAGE-SCOPE

--- 01-package-scope/main.go
+++ 01-package-scope/main.go
@@ -8,10 +8,15 @@ var a string = "Mazen"
 func main() {
 	fmt.Println(x)
 	foo()
+	max := max (7)
+	fmt.Println("Maximum = ", max)
 }
 
 func foo() {
 	fmt.Println(x)
 	fmt.Println(a)
+}
+func max(x int) int {
+	return 42 + x
 }
 

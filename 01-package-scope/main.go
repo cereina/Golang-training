110ac1ad463baf3a0d844f8762e21befbcce9111
cereina <kassemmazen@gmail.com>
2017-07-16 17:27:02 -0400

    ADD FILE 01-PACKAGE-SCOPE

--- 01-package-scope/main.go
+++ 01-package-scope/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 var x int = 42
+var a string = "Mazen"
 
 func main() {
 	fmt.Println(x)
@@ -11,5 +12,6 @@ func main() {
 
 func foo() {
 	fmt.Println(x)
+	fmt.Println(a)
 }
 
